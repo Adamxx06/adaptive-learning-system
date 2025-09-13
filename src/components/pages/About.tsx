de40c208213660d0c6e3ccd6cf5c4a57125f47e9
Adamxx06 <adamsblessing637@gmail.com>
2025-09-13 01:41:27 +0100

    feat(about): update about page content

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,8 +1,15 @@
+
 const About = () => {
   return (
-    <div className="container">
-      <h1>About CodeAdapt</h1>
-      <p>Welcome to CodeAdapt! Learn and adapt faster 🚀</p>
+    <div className="about-page">
+    <main className="container my-5" aria-label="About Us">
+      <div className="container">
+        <header className="text-center">
+          <h1>About Us</h1>
+          <p>Discover how we make coding simple, personalized, and engaging. At CodeAdapt, we’re passionate about empowering learners with an adaptive platform that helps you grow your skills at your own pace.</p>
+        </header>
+      </div>
+    </main>
     </div>
   );
 };
